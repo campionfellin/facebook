88875281642bc9715a5feb0aa915a488a85e0858
Campion Fellin <campionfellin@Campions-MacBook-Pro.local>
2016-04-15 14:19:54 +0200

    get their name

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function getName(sender) {
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
         } else {
-            console.log("blahdata: " + body.first_name)
+            sendTextMessage("Hello, " + body.first_name + ". Or should I call you Mr./Mrs. " + body.last_name + "?")
         }
     })
 
