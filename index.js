0060a1781b906cb03f9e85ca6a3a848c8d5da32e
Campion Fellin <campionfellin@Campions-MacBook-Pro.local>
2016-04-15 15:47:24 +0200

    wit

--- index.js
+++ index.js
@@ -215,24 +215,59 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
+'use strict';
+
+// Quickstart example
+// See https://wit.ai/l5t/Quickstart
+
+// When not cloning the `node-wit` repo, replace the `require` like so:
+// const Wit = require('node-wit').Wit;
 const Wit = require('node-wit').Wit;
- 
+
+const token = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/weather.js <wit-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
 const actions = {
   say: (sessionId, msg, cb) => {
     console.log(msg);
     cb();
   },
   merge: (context, entities, cb) => {
+    // Retrieve the location entity and store it into a context field
+    const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      context.loc = loc;
+    }
     cb(context);
   },
   error: (sessionId, msg) => {
     console.log('Oops, I don\'t know what to do.');
   },
-  'my-action': (context, cb) => {
-    context['name'] = 'Julien';
+  'fetch-weather': (context, cb) => {
+    // Here should go the api call, e.g.:
+    // context.forecast = apiCall(context.loc)
+    context.forecast = 'sunny';
     cb(context);
   },
 };
- 
-const client = new Wit('VZUVI3OXYGU3ABATDOC3J5GTGV3NR5MK', actions);
+
+const client = new Wit(token, actions);
 client.interactive();
+
