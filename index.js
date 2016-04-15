1a3203cc877a6145880552b477da71126564f690
Campion Fellin <campionfellin@Campions-MacBook-Pro.local>
2016-04-15 14:09:31 +0200

    get their name

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function getName(sender) {
         } else if (response.body.error) {
             console.log('Error: ', response.body.error)
         } else {
-            console.log("blahdata: " + body.response)
+            console.log("blahdata: " + body.first_name)
         }
     })
 
