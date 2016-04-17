8eb75d46e5d5e38ec4be895b55f5fb7cd3dc0287
Campion Fellin <campionfellin@Campions-MacBook-Pro.local>
2016-04-17 12:59:24 +0200

    wit again

--- index.js
+++ index.js
@@ -143,6 +143,28 @@ const actions = {
   error: (sessionId, error) => {
     console.log(error.message);
   },
+
+  say: (sessionId, context, message, cb) => {
+    console.log(message);
+    cb();
+  },
+  merge: (sessionId, context, entities, message, cb) => {
+    // Retrieve the location entity and store it into a context field
+    const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      context.loc = loc;
+    }
+    cb(context);
+  },
+  error: (sessionId, context, error) => {
+    console.log(error.message);
+  },
+  'fetch-weather': (sessionId, context, cb) => {
+    // Here should go the api call, e.g.:
+    // context.forecast = apiCall(context.loc)
+    context.forecast = 'sunny';
+    cb(context);
+  },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
 };
